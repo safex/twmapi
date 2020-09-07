1552993c3f5c87abdae1b1e0f01ed6a86f3ecc96
dally <xordaniel@gmail.com>
2020-09-07 01:15:32 -0400

    add admin api port to config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const API_PORT = 17700;
+const ADMIN_API_PORT = 18800;
 
 const WALLET_PATH = '';
 const WALLET_PASSWORD = '';
@@ -6,5 +7,6 @@ const SAFEXD_RPC_PORT = 0;
 const SAFEXD_RPC_HOST = '';
 
 module.exports = {
-    API_PORT
+    API_PORT,
+    ADMIN_API_PORT
 }
